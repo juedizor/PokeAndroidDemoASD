055df4131533f4a803ef3c3b797ac73139159baa
juedizor <juedizor@gmail.com>
2017-01-18 07:37:09 -0500

    cambios
    prueba pull

--- app/src/main/java/co/com/grupoasd/pokedexdemoasd/adapter/AdaptadorItem.java
+++ app/src/main/java/co/com/grupoasd/pokedexdemoasd/adapter/AdaptadorItem.java
@@ -14,7 +14,7 @@ import co.com.grupoasd.pokedexdemoasd.object.Item;
 /**
  * Created by ASD on 28/12/2016.
  */
-
+// comentario
 public class AdaptadorItem extends ArrayAdapter {
 
     Item[] datos;
